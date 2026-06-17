d7b6271b615d13d40fb4ed9a69c3faaff1a90832
comic-tech <victorthekid37@gmail.com>
2026-06-17 14:59:00 +0300

    Refactor shred function for improved file security
    Updated shred function to securely delete files by encrypting and renaming them before removal. Enhanced versioning and added functionality for directory handling.

--- shredder_1.1.py
+++ shredder_1.1.py
@@ -1,23 +1,25 @@
-#version 1.1
+#version 1.2
+#delete files securely and leave minimal traces of the file including file name which may give out some info like date created 
 
 import os
 from cryptography.fernet import Fernet
 
-def main():
+#encrypt + delete with different keys for each file
+def shred(f_path):
     key = Fernet.generate_key()
-
-    path = input ("Enter file path: ")
-
     try:
-        with open(path, "rb") as file:
+        with open(f_path, "rb") as file:
             data = file.read()
 
         contents = Fernet(key).encrypt(data)
 
-        with open(path, "wb") as file:
+        with open(f_path, "wb") as file:
             file.write(contents)
 
-        os.remove(path)
+        new = Fernet(key).encrypt(f_path.encode())
+        os.rename(f_path, new)
+        os.remove(new)
+        print(f"DELETED : {f_path}")
 
 
     except Exception as e:
@@ -25,7 +27,19 @@ def main():
 
     finally:
         key = None
-        print("done")
+        
+
+def main():
+    f_path = input ("Path:- ")
+    if os.path.isdir(f_path):
+        for i in os.listdir(f_path):
+            shred(os.path.join(f_path,i))
+        os.rmdir(f_path)
+        print("DONE\n")
+    
+    else:
+        shred(f_path)
+        print("DONE\n")
 
 if __name__ == "__main__":
     main()
